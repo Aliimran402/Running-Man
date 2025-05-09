9b5962e1a28eb9b481b799822d3cc7360f485cf8
Shafin1814 <xzsafin1814@gmail.com>
2025-05-09 22:33:08 +0600

    Updated draw_text

--- Running Man.py
+++ Running Man.py
@@ -169,7 +169,26 @@ def generate_path_segment():
         })
     
     
-def draw_text(): 
+def draw_text(x, y, text, font=GLUT_BITMAP_HELVETICA_18):
+    glColor3f(*COLORS['text'])
+    glMatrixMode(GL_PROJECTION)
+    glPushMatrix()
+    glLoadIdentity()
+    
+    gluOrtho2D(0, 1000, 0, 800)
+    
+    glMatrixMode(GL_MODELVIEW)
+    glPushMatrix()
+    glLoadIdentity()
+    
+    glRasterPos2f(x, y)
+    for ch in text:
+        glutBitmapCharacter(font, ord(ch))
+    
+    glPopMatrix()
+    glMatrixMode(GL_PROJECTION)
+    glPopMatrix()
+    glMatrixMode(GL_MODELVIEW)
     
        
 
