d2f7f3dfe8c6a80381974da1f73945cd9de78d39
Shafin1814 <xzsafin1814@gmail.com>
2025-05-09 20:57:29 +0600

    Added draw _text to the game

--- Running Man.py
+++ Running Man.py
@@ -103,6 +103,25 @@ def generate_path_segment():
     
     
 def draw_text(): 
+    glColor3f(*COLORS['text'])
+    glMatrixMode(GL_PROJECTION)
+    glPushMatrix()
+    glLoadIdentity()
+    
+    gluOrtho2D(0, 1000, 0, 800)
+    
+    glMatrixMode(GL_MODELVIEW)
+    glPushMatrix()
+    glLoadIdentity()
+    
+    glRasterPos2f(x, y)
+    for ch in text:
+        glutBitmapCharacter(font, ord(ch))
+    
+    glPopMatrix()
+    glMatrixMode(GL_PROJECTION)
+    glPopMatrix()
+    glMatrixMode(GL_MODELVIEW)
     
        
 
